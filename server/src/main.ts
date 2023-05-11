390c9a62d8491f405e56debbfb65e847412c99d8
0xRnato <rnato@arch.localdomain>
2023-05-11 17:45:18 -0300

    feat(backend): creating database and setting up prisma

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './database/prisma.service';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
+  console.log('running at: ', port);
 }
 bootstrap();
