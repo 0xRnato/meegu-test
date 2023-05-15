56f025e4606b7e77a16299e29fe1c24614b86432
Renato Neto <rnato.netoo@gmail.com>
2023-05-15 17:40:39 -0300

    fix(frontand): minor change to get the actual object data

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -2,23 +2,23 @@ import { ICreateUser, IUpdateUser, IUser } from '@/types/user';
 import api from './api';
 
 const createUser = async (userData: ICreateUser): Promise<IUser> => {
-  const { data } = await api.post('/user', userData);
-  return data;
+  const { data: result } = await api.post('/user', userData);
+  return result.data;
 };
 
 const getUsers = async (): Promise<IUser[]> => {
-  const { data } = await api.get('/user');
-  return data;
+  const { data: result } = await api.get('/user');
+  return result.data;
 };
 
 const getUserById = async (userId: number): Promise<IUser> => {
-  const { data } = await api.get(`/user/${userId}`);
-  return data;
+  const { data: result } = await api.get(`/user/${userId}`);
+  return result.data;
 };
 
 const updateUser = async (userId: number, userData: IUpdateUser): Promise<IUser> => {
-  const { data } = await api.patch(`/user/${userId}`, userData);
-  return data;
+  const { data: result } = await api.patch(`/user/${userId}`, userData);
+  return result.data;
 };
 
 const deleteUser = async (userId: number): Promise<void> => {
