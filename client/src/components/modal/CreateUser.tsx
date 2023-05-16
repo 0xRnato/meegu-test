df6a73308c63bf55de644d053a42eac4bb512e04
Renato Neto <rnato.netoo@gmail.com>
2023-05-16 00:24:55 -0300

    feat(frontend): datepicker added for the birthdate

--- client/src/components/modal/CreateUser.tsx
+++ client/src/components/modal/CreateUser.tsx
@@ -1,7 +1,9 @@
-'use client';
+import { useState } from 'react';
+import { SubmitHandler, useForm, Controller } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import { ICreateUser } from '@/types/user';
-import { SubmitHandler, useForm } from 'react-hook-form';
 import { useUser } from '@/contexts/UserContext';
 
 interface ICreateUserProps {
@@ -11,15 +13,21 @@ interface ICreateUserProps {
 
 export default function CreateUser({ modalId, closeModal }: ICreateUserProps) {
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<ICreateUser>();
   const { createUser } = useUser();
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const onSubmit: SubmitHandler<ICreateUser> = async (data: ICreateUser) => {
+  const onSubmit: SubmitHandler<ICreateUser> = async (data) => {
     await createUser(data);
     closeModal();
+  };
+
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
   };
 
   return (
@@ -32,44 +40,59 @@ export default function CreateUser({ modalId, closeModal }: ICreateUserProps) {
             className={`input input-primary ${errors.name ? 'input-error' : ''}`}
             {...register('name', { required: 'Name is required' })}
           />
+          {errors.name && <span className="error">{errors.name.message?.toString()}</span>}
         </label>
 
         <label className="label">
-          birthdate
-          <input
-            type="text"
-            className={`input input-primary ${errors.birthdate ? 'input-error' : ''}`}
-            {...register('birthdate', { required: 'birthdate is required' })}
-          />
+          Birthdate
+          <div>
+            <Controller
+              control={control}
+              name="birthdate"
+              rules={{ required: 'Birthdate is required' }}
+              render={({ field }) => (
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => {
+                    handleDateChange(date as Date);
+                    field.onChange(date);
+                  }}
+                  peekNextMonth
+                  showMonthDropdown
+                  showYearDropdown
+                  dropdownMode="select"
+                  className={`input input-primary ${errors.birthdate ? 'input-error' : ''}`}
+                />
+              )}
+            />
+          </div>
+          {errors.birthdate && <span className="error">{errors.birthdate.message?.toString()}</span>}
         </label>
 
         <label className="label">
-          document
+          Document
           <input
             type="text"
             className={`input input-primary ${errors.document ? 'input-error' : ''}`}
-            {...register('document', { required: 'document is required' })}
+            {...register('document', { required: 'Document is required' })}
           />
+          {errors.document && <span className="error">{errors.document.message?.toString()}</span>}
         </label>
 
         <label className="label">
-          zipcode
+          Zipcode
           <input
             type="text"
             className={`input input-primary ${errors.zipcode ? 'input-error' : ''}`}
-            {...register('zipcode', { required: 'zipcode is required' })}
+            {...register('zipcode', { required: 'Zipcode is required' })}
           />
         </label>
 
         <label className="label">
-          Tarms and Conditions
+          Terms and Conditions
           <input type="checkbox" className="checkbox checkbox-primary" {...register('acceptedTermsAndConditions')} />
+          {errors.zipcode && <span className="error">{errors.zipcode.message?.toString()}</span>}
         </label>
-
-        {errors.name && <span className="error">{errors.name.message?.toString()}</span>}
-        {errors.birthdate && <span className="error">{errors.birthdate.message?.toString()}</span>}
-        {errors.document && <span className="error">{errors.document.message?.toString()}</span>}
-        {errors.zipcode && <span className="error">{errors.zipcode.message?.toString()}</span>}
       </div>
       <div className="modal-action">
         <button className="btn btn-primary" type="submit">
